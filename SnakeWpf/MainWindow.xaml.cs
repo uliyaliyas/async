88ccd68e0e8c20721c864ab2378b4eb0c0f44ece
FunnerP <finnerkine@gmail.com>
2024-04-27 14:11:25 +0200

    snake

---
+++ SnakeWpf/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Ellipse figure = new Ellipse();
+            figure.Width = 100;
+            figure.Height = 100;
+            figure.Fill = Brushes.Black;
+            figure.Stroke = Brushes.Blue;
+            figure.StrokeThickness = 3;
+            OurCanvas.Children.Add(figure);
+            OurCanvas.Children[0].
+        }
+    }
+}
